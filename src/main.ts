564d186aef4b474f67897a688b0ecba65f23a2c3
MichaelG48 <michael_glz48@outlook.es>
2022-12-09 09:19:42 -0600

    Inicial

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,21 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
+import '@angular/service-worker';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  window.addEventListener("load", () => {
+    registerSW();
+  });
+  
+  async function registerSW() {
+    if ('serviceWorker' in navigator) {
+      try {
+        await navigator.serviceWorker.register('safety-worker.js', { scope: '/' });
+      } catch (e) {
+        console.log(`SW registration failed`);
+      }
+    }
+  }
